041f583da192fa24928438a60a345076546a0988
duaghwns <hj2758@gmail.com>
2022-04-16 00:58:22 +0900

    컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React, { Component} from 'react';
 import './App.css';
 
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+            <h1>WEB</h1>
+            world wide web!
+        </header>
+    );
+  }
+}
 class App extends Component {
   render() {
     return (
