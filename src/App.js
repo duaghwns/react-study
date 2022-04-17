b92a68752ae21d5d9a28ad89b057c44d7be1e991
duaghwns <hj2758@gmail.com>
2022-04-17 18:11:36 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import React, { Component} from 'react';
 import './App.css';
 
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-            <h1>WEB</h1>
-            world wide web!
-        </header>
-    );
-  }
-}
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <a href="pure.html">pure</a>
+      <Subject title="HTML" sub="world wide web!"></Subject>
+      <Nav></Nav>
+      <Content title="HTML" desc="HTML is Hypertext Markup Language."></Content>
     </div>
     );
+  }
+}
+
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+            <h1>{this.props.title}</h1>
+            {this.props.sub}
+        </header>
+    );
+  }
+}
+
+class Nav extends Component {
+  render() {
+    return (
+      <nav>
+            <ul>
+                <li><a href="1.html">HTML</a></li>
+                <li><a href="2.html">CSS</a></li>
+                <li><a href="3.html">JavaScript</a></li>
+            </ul>
+        </nav>
+    )
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>
+            <h2>{this.props.title}</h2>
+            {this.props.desc}
+        </article>
+    )
   }
 }
 
