f295a4a9faa3068a3d4bc09735f5ca6a62cba2f3
duaghwns <hj2758@gmail.com>
2022-04-18 00:45:21 +0900

    about state

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,13 +1,18 @@
 import React, { Component} from 'react';
 
 class TOC extends Component {
-    render() {
+  render() {
+      const data = this.props.data;
+      let i =0;
+      let list = [];
+      while(i<data.length) {
+        list.push(<li key={data[i].id}><a href={"/content/" + data[i].id}>{data[i].title}</a></li>);
+        i++;
+      }
       return (
         <nav>
               <ul>
-                  <li><a href="1.html">HTML</a></li>
-                  <li><a href="2.html">CSS</a></li>
-                  <li><a href="3.html">JavaScript</a></li>
+                  {list}
               </ul>
           </nav>
       )
