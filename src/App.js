b9276997432c1c6eb1906e3128afa9c616756c28
duaghwns <hj2758@gmail.com>
2022-04-22 00:01:37 +0900

    17.1 컴포넌트 이벤트 만들기

--- src/App.js
+++ src/App.js
@@ -32,24 +32,16 @@ class App extends Component {
     }
     return (
       <div className="App">
-      {/*<Subject 
+      <Subject 
         title={this.state.subject.title} 
-        sub={this.state.subject.sub}>
-      </Subject>*/}
-      <header>
-        <h1>
-          <a href="/" onClick={function(e) {
-            e.preventDefault();
-            // this.state.mode = 'welcome';
-            this.setState({
-              mode: 'welcome'
-            });
-          }.bind(this)}>
-            {this.state.subject.title}
-          </a>
-        </h1>
-        {this.state.subject.sub}
-      </header>
+        sub={this.state.subject.sub}
+        onChangePage={function(){
+          this.setState({
+            mode:'welcome'
+          });
+        }.bind(this)} 
+        >
+      </Subject>
       <TOC data={this.state.content}></TOC>
       <Content title={_title} desc={_content}></Content>
     </div>
