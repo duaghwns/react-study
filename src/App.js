c5ce4b34bc9c00a888a32c8f2a218da4d1c93108
duaghwns <hj2758@gmail.com>
2022-06-23 21:35:26 +0900

    d

--- src/App.js
+++ src/App.js
@@ -1,59 +1,16 @@
-import React, { Component} from 'react';
-import TOC from './components/TOC';
-import Content from './components/Content';
-import Subject  from './components/Subject';
 import './App.css';
+import Nav from './pages/Nav'
+import Register from './pages/Register'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      mode:'welcome',
-      subject:{title:'WEB', sub:'World Wide Web!'},
-      welcome:{title:'Welcome', desc:'Hello, React!'},
-      content:[
-        {id:1, title: 'HTML', desc: 'HTML is for information'},
-        {id:2, title: 'CSS', desc: 'CSS is for design'},
-        {id:3, title: 'JavaScript', desc: 'JavaScript is for interactive'},
-      ]
-    }
-  }
-    
-    render() {
-    let _title, _content = null;
-    if(this.state.mode === 'welcome'){
-      _title = 'Welcome';
-      _content = 'Welcome to React!';
-    } else if(this.state.mode === 'read'){
-      _title = this.state.content[0].title;
-      _content = this.state.content[0].desc;
-    }
-    return (
-      <div className="App">
-      <Subject 
-        title={this.state.subject.title} 
-        sub={this.state.subject.sub}
-        onChangePage={function(){
-          this.setState({
-            mode:'welcome'
-          });
-        }.bind(this)} 
-        >
-      </Subject>
+function App() {
+  return (
+    <div className="App">
 
-      <TOC data={this.state.content}
-           onChangePage = {function(){
-             this.setState({
-               mode:'read'
-             })
-           }.bind(this)}
-        >
-      </TOC>
-      
-      <Content title={_title} desc={_content}></Content>
+      <Nav/>
+      <Register/>
+
     </div>
-    );
-  }
+  );
 }
 
 export default App;
---
+++ src/pages/Nav.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+
+
+function Nav(){
+    let [state,setState] = useState()
+
+    return (
+        <div className="nav-container">
+            
+        </div>
+    )
+}
+
+export default Nav;
